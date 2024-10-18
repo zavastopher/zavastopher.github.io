ea9e80571b4a6f390d7937de432d4a56ecbf72cc
Chris <zavastopher@gmail.com>
2024-10-18 00:11:15 -0400

    Added tailwind and currently editing landing page

---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+        colors: {
+            'dark': '#0E121D',
+            'light': '#FFFFFF',
+            'middle': '#465071',
+            'accent1': '#78CA9D',
+            'accent2': '#D491B4',
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        fontSize: {
+            sm: '.75rem',
+            base: '1rem',
+            xl: '1.25rem',
+            '2xl': '1.75rem',
+            '3xl': '2.5rem',
+            '4xl': '3.75rem',
+            '5xl':  '6.25rem',
+            '6xl': '10rem',
+            '7xl': '15rem',
+        },
+		extend: {},
+	},
+	plugins: [],
+}
