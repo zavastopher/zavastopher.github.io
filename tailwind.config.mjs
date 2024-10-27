6026a5701edf52948dbc187332433154ce957ef4
Chris <zavastopher@gmail.com>
2024-10-27 13:05:07 -0400

    Content update to try to fix tailwind.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./public/**/*.html','./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
         colors: {
             'dark': '#0E121D',
